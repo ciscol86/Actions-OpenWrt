2def202e9e859fa2cc6020cdac037b3629b21e48
ciscol86 <51709658+ciscol86@users.noreply.github.com>
2023-04-04 09:54:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改默认IP为 192.168.8.5
+sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
+ 
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Ciscol_Router'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /Ciscol_Guan build V1.0 @ OpenWrt Lede /g" package/lean/default-settings/files/zzz-default-settings
